b0cd42251ed6ac0e14a2da2974a74aecca214103
Nacho Colomina <ignacio.colomina@gmail.com>
2025-01-11 17:44:26 +0100

    Adapt simple deposit to protocol 22

--- simple_deposit/src/test.rs
+++ simple_deposit/src/test.rs
@@ -6,65 +6,35 @@ use token::Client as TokenClient;
 use token::StellarAssetClient as TokenAdminClient;
 
 fn create_token_contract<'a>(e: &Env, admin: &Address) -> (TokenClient<'a>, TokenAdminClient<'a>) {
-    let contract_address = e.register_stellar_asset_contract(admin.clone());
+    let sac = e.register_stellar_asset_contract_v2(admin.clone());
     (
-        TokenClient::new(e, &contract_address),
-        TokenAdminClient::new(e, &contract_address),
+        TokenClient::new(e, &sac.address()),
+        TokenAdminClient::new(e, &sac.address()),
     )
 }
 
-struct TestData<'a> {
-    admin: Address,
-    user: Address,
-    client:  CryptoDepositClient<'a>,
-    token: TokenClient<'a>,
-    token_admin: TokenAdminClient<'a>
-}
-
-fn init_test_data(e: &Env) -> TestData {
+fn create_contract<'a>(e: &'a Env, amount: &'a i128) -> (CryptoDepositClient<'a>, Address) {
     e.mock_all_auths();
-
-    let contract_id = e.register_contract(None, CryptoDeposit);
-    let client = CryptoDepositClient::new(&e, &contract_id);
 
     let admin = Address::generate(&e);
     let user = Address::generate(&e);
     let (token, token_admin) = create_token_contract(&e, &admin);
+    token_admin.mint(&user, &amount);
 
-    TestData {
-        admin,
-        user,
-        client,
-        token,
-        token_admin
-    }
-}
+    let client = CryptoDepositClient::new(
+        e,
+        &e.register(
+            CryptoDeposit {}, 
+            (admin, token.address)
+        )
+    );
 
-#[test]
-fn test_init() {
-    let e = Env::default();
-    let test_data = init_test_data(&e);
-    assert_eq!(test_data.client.init(&test_data.admin, &test_data.token.address), true);
+    (client, user.clone())
 }
 
 #[test]
 fn test_deposit() {
     let e = Env::default();
-    let test_data = init_test_data(&e);
-    test_data.token_admin.mint(&test_data.user, &100);
-
-    test_data.client.init(&test_data.admin, &test_data.token.address);
-    assert_eq!(test_data.client.deposit(&test_data.user, &50), 50);
-}
-
-#[test]
-#[should_panic(expected = "HostError: Error(Contract, #2)")]
-fn test_fail_not_initialized() {
-    let e = Env::default();
-    e.mock_all_auths();
-    let test_data = init_test_data(&e);
-
-    let contract_id = e.register_contract(None, CryptoDeposit);
-    let crypto_bills_client = CryptoDepositClient::new(&e, &contract_id);
-    assert_eq!(crypto_bills_client.deposit(&test_data.user, &50), 50);
+    let test_data = create_contract(&e, &100_i128);
+    assert_eq!(test_data.0.deposit(&test_data.1, &50), 50);
 }
